8f04d148a2645c07926c8092c1e2b3a31c8c2316
Mahmuda Tanjin <mishuandmiju@gmail.com>
2023-03-30 17:15:40 +0600

    learning list, tuple, func

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+
+
+z = float(3)
+
+
+def a():
+    print(z)
+a()
+list = ["apple", "banana", "cherry"]
+print(list)
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
